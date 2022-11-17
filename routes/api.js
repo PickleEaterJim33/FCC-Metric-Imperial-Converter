95aed40abbe7b7d157c29b97664a46e29aa3eb10
Jan Mlinarić <jan.mlinaric@gmail.com>
2022-11-17 22:10:20 +0100

    fix: recognise both invalid inputs in /api/convert

--- routes/api.js
+++ routes/api.js
@@ -7,22 +7,33 @@ module.exports = function (app) {
   
   app.route('/api/convert').get((req, res) => {
     const input = req.query.input;
-    let result;
+    let result = {};
 
+    let numberError = false, unitError = false, errorMsg;
     try {
-      result = {
-        initNum: convertHandler.getNum(input),
-        initUnit: convertHandler.getUnit(input)
-      };
-      result.returnNum = convertHandler.convert(result.initNum, result.initUnit);
-      result.returnUnit = convertHandler.getReturnUnit(result.initUnit);
-      result.string = convertHandler.getString(
-        result.initNum, result.initUnit,
-        result.returnNum, result.returnUnit
-      );
+      result.initNum = convertHandler.getNum(input);
     } catch (error) {
-      result = error.message;
+      numberError = true;
+      errorMsg = error.message;
     }
+    try {
+      result.initUnit = convertHandler.getUnit(input);
+    } catch (error) {
+      unitError = true;
+      errorMsg = error.message;
+    }
+    if (numberError) {
+      return res.send(unitError ? "invalid number and unit" : errorMsg);
+    } else if (unitError) {
+      return res.send(errorMsg);
+    }
+
+    result.returnNum = convertHandler.convert(result.initNum, result.initUnit);
+    result.returnUnit = convertHandler.getReturnUnit(result.initUnit);
+    result.string = convertHandler.getString(
+      result.initNum, result.initUnit,
+      result.returnNum, result.returnUnit
+    );
 
     res.send(result);
   });
