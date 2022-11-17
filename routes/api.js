b81f03649f6dd0d2ebe5694637023738ce372cf2
Jan Mlinarić <jan.mlinaric@gmail.com>
2022-11-17 21:42:04 +0100

    feat: complete api.js

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,25 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
   
-  
+  app.route('/api/convert').get((req, res) => {
+    const input = req.query.input;
+    let result;
+
+    try {
+      result = {
+        initNum: convertHandler.getNum(input),
+        initUnit: convertHandler.getUnit(input)
+      };
+      result.returnNum = convertHandler.convert(result.initNum, result.initUnit);
+      result.returnUnit = convertHandler.getReturnUnit(result.initUnit);
+      result.string = convertHandler.getString(
+        result.initNum, result.initUnit,
+        result.returnNum, result.returnUnit
+      );
+    } catch (error) {
+      result = error.message;
+    }
+
+    res.send(result);
+  });
 };
